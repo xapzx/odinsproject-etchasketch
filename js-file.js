8352a644d5a551510a177b3f57a15cd64636308c
Andy <andy@test.com>
2022-02-13 20:56:41 +1030

    Implemented JS to create a 16x16 square divs

---
+++ js-file.js
@@ -0,0 +1,14 @@
+const main = document.querySelector(".main-container");
+let count = 0;
+
+for(let i = 0; i < 16; i++){
+    let vertical = document.createElement('div')
+    vertical.classList.add('squareRow');
+    for(let j = 0; j < 16; j++){
+        let horizontal = document.createElement('DIV');
+        horizontal.classList.add('squareCol');
+        vertical.appendChild(horizontal);
+        horizontal.textContent = count++;
+    }
+    main.appendChild(vertical);
+}
