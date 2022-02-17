b44ee9b96e3a53a0591da0e1f55927d98aca4612
Andy <andy@test.com>
2022-02-17 17:51:26 +1030

    implemented to allow drawing on the grid with black and allow resetting to grid to blank

--- js-file.js
+++ js-file.js
@@ -8,7 +8,26 @@ for(let i = 0; i < 16; i++){
         let horizontal = document.createElement('DIV');
         horizontal.classList.add('squareCol');
         vertical.appendChild(horizontal);
-        horizontal.textContent = count++;
+        // horizontal.textContent = count++;
+        // horizontal.addEventListener('mouseover', function(event){
+        //     event.target.style.backgroundColor = 'black';
+        // })
     }
     main.appendChild(vertical);
 }
+
+//Event listener to allow drawing on the grid
+const hover = document.querySelector('.mc-border');
+hover.addEventListener("mouseover", function(event) {
+    // highlight the mouseover target
+    event.target.style.background = "black";
+});
+
+//Clear the grid
+const clear = document.querySelector('.reset');
+clear.addEventListener("click", function() {
+    let squares = document.querySelectorAll("div.squareCol");
+    squares.forEach(function(square) {
+        square.style.background = 'white';
+    })
+})
